35373d1451fbfe92a365add52efb542c90095ab6
Bayan Assali <84909990+Bonz3@users.noreply.github.com>
2021-05-29 20:04:29 +0400

    Made changes to the code
    - Made the delay code into a separate function 
    - Created the "clicking" function

--- case1_code/case1_code.ino
+++ case1_code/case1_code.ino
@@ -1,7 +1,8 @@
 # include "M5Atom.h"
 const int Interval = 500; // time interval between blinks
 unsigned long timeSinceBlink = 0; // holds time since begin
-uint32_t white = 0xFFFFFF; //white hex num
+uint32_t white = 0xffffff; //white hex num
+uint32_t red = 0xff0000; 
 const int pixel_num = 25; // number of pixels 5x5
 int ledState = LOW;  
 
@@ -9,20 +10,33 @@ int ledState = LOW;
 void setup() {
   M5.begin(true,false, true); 
 }
+uint8_t FSM = 0;
 
-void loop() {
-  //case 1 - not added the clicking function
-  for(int i = 0; i < pixel_num; i++)
-    M5.dis.drawpix(i,white); // changes pixel to white
 
+void interval_delay(){ // function delay
   unsigned long currentTime = millis();
-  
   if (currentTime - timeSinceBlink >= Interval) {
     timeSinceBlink = currentTime; // save the last time you blinked the LED
-    if (ledState == LOW) {
+    if (ledState == LOW)
       ledState = HIGH;
-    } else {
+    else
       ledState = LOW;
+ }
+}
+  
+void loop() {
+  if(M5.Btn.wasPressed()){
+    switch (FSM){
+      case 0: // Manual Rear strobe (RED)
+        for(int i = 0; i < pixel_num; i++)
+          M5.dis.drawpix(i,red); // changes pixel to red
+        interval_delay();
+        break;
+      case 1: // Manual Rear strobe (WHITE)
+        for(int i = 0; i < pixel_num; i++)
+          M5.dis.drawpix(i,white); // changes pixel to red
+        interval_delay();
+        break;
     }
   }
 }
