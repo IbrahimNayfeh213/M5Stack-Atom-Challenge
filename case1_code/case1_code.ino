52a869f3e27eee6f50cab6da9ba028f9ba44c062
Bonz3 <ba2173@nyu.edu>
2021-05-30 09:55:51 +0400

    removed header and made some other edits

--- case1_code/case1_code.ino
+++ case1_code/case1_code.ino
@@ -1,11 +1,11 @@
 # include "M5Atom.h"
-#include "millisDelay.h" 
 
 /*Acceleration variables in the x, y, and z directions*/
 float accX, accY, accZ;
-
+float accXAvg;
+int n;
 /*Time interval for blinks*/
-const unsigned long Interval = 1000; 
+unsigned long Interval = 1000; 
 /*Time passed for intervalDelay function*/
 unsigned long previousT = 0; 
 
@@ -34,63 +34,76 @@ void fillScreen(uint32_t color){
 uint8_t FSM = 0;
 
 /*Time Delay function */
-void intervalDelay(){  
+void intervalDelay(uint32_t color){  
   unsigned long currentT = millis();
   if(currentT - previousT >= Interval){ // if time from start reached 1000 mili seconds the screen will be filled with black 
      fillScreen(black);
      previousT = currentT; // updates time for the next loop
+     fillScreen(color); // fills the screen back if the original color
   }
+  
 }
   
 void loop() {
+  uint32_t currentColor = black;
   if(M5.Btn.wasPressed()){
     switch (FSM){
       case 0: //  OFF 
-          fillScreen(black); // changes pixel to black
+          fillScreen(currentColor); // changes pixel to black
         break;
       case 1: // Manual Rear strobe (RED)
-          CaseRed();
+          currentColor = red;
+          fillScreen(currentColor);
         break;
       case 2: // Manual Rear strobe (WHITE)
-          CaseWhite();
+          currentColor = white;
+          fillScreen(currentColor);
         break;
-    /*case 3:
-          CaseRed();
-          M5.IMU.getAccelData(&accX, &accY, &accZ);
-          if(accY ...)
-            fillScreen(red);
-      case 4:
-          CaseWhite();
-          M5.IMU.getAccelData(&accX, &accY, &accZ);
-          if(accY ...)
-            fillScreen(white);
-          */
-          
-        default:
+      case 3: // Automatic Rear Mode Rear (RED)
+          currentColor = red;
+          fillScreen(currentColor);
+      case 4: // Automatic Rear Mode Rear (WHITE)
+          currentColor = white;
+          fillScreen(currentColor);
+      default:
             break;
     }
     FSM++;
-        if (FSM >= 2)
-        {
-            FSM = 0;
-        }
+    if (FSM >= 5)
+       FSM = 0;
   }
-  delay(50);
+  
+  intervalDelay(currentColor);
+  
+//  if(FSM == 3 || FSM == 4){
+//    M5.IMU.getAccelData(&accX, &accY, &accZ);
+//    float initalAcc = *accX;
+//    for(int i = 0; i < n;i++){
+//      
+//      
+//      
+//    }
+//    float initalAcc = ;
+//    accXAvg = ((accXAvg * (n-1))+accX)/n;
+//    if( - <= -0.115)
+//       fillScrean(currentColor);
+//  }
+  
   M5.update();
 }
 
-/*Shows Blinking Red Screen */
-void CaseRed(){
-   fillScreen(red); // changes pixel to red
-   intervalDelay();
-  
-}
-
-/*Shows Blinking White Screen */
-void CaseWhite(){
-    fillScreen(white); // changes pixel to white
-    intervalDelay();
-}
+///*Shows Blinking Red Screen */
+//void CaseRed(){
+//   fillScreen(red); // changes pixel to red
+//   intervalDelay();
+//  
+//}
+//
+///*Shows Blinking White Screen */
+//void CaseWhite(){
+//    fillScreen(white); // changes pixel to white
+//    intervalDelay();
+//}
 
 
 // possibly for acceleration
