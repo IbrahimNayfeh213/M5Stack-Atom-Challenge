05e32d24c56033dcabeb5a6ecb13dc06d55ba74d
IbrahimNayfeh213 <84908074+IbrahimNayfeh213@users.noreply.github.com>
2021-05-31 13:50:29 +0400

    Removed testing part of the acc. / brightness

--- case1_code/case1_code.ino
+++ case1_code/case1_code.ino
@@ -86,31 +86,3 @@ void loop() {
   
     M5.update();
 }
-
-// possibly for acceleration
-//void setup() {
-//  oldX = -1;
-//  oldY = -1;
-//}
-//
-//void loop() {
-//  float accX, accY, accZ;
-//  int x, y;
-//
-//  M5.IMU.getAccelData(&accX, &accY, &accZ);
-//  x = constrain(accX * 5 + 2, 0, 4);
-//  y = constrain(accY * 5 + 2, 0, 4);
-//  if (oldX != x || oldY != y) {
-//    M5.dis.drawpix(oldX, oldY, 0x000000);
-//    M5.dis.drawpix(x, y, 0xffffff);
-//    oldX = x;
-//    oldY = y;
-//  }
-//}
-//
-
-//test
-//Function to change brightness of the LEDs
-//void setBrightness(uint8_t brightness){
-//  brightness=20;
-//}
