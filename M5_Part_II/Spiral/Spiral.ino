ea8e97b9d74bc31faba62649ac1d6aebf568dca9
G-Kebede <65799321+G-Kebede@users.noreply.github.com>
2021-06-06 15:13:16 +0400

    Added an exhale proces, added color changing on each bar, slowed time for breathing to be more calming.

--- M5_Part_II/Spiral/Spiral.ino
+++ M5_Part_II/Spiral/Spiral.ino
@@ -3,16 +3,17 @@
 
 uint32_t black = 0x0000; 
 uint32_t blue1 = 0x0000ff; // blue color to test
-uint32_t L1color = 0xD1EEEE;  // light cyan2
-uint32_t L2color = 0xADEAEA;  // turquoise
-uint32_t L3color = 0x70DBDB;  // medium turquoise
-uint32_t L4color = 0x00CED1;  // dark turquoise (SVG)
-uint32_t L5color = 0x00868B;  // turquoise4
+uint32_t L1color = 0xccccff;  // light cyan2
+uint32_t L2color = 0x9999ff;  // turquoise
+uint32_t L3color = 0x6666ff;  // medium turquoise
+uint32_t L4color = 0x3232ff;  // dark turquoise (SVG)
+uint32_t L5color = 0x0000ff;  // turquoise4
 millisDelay Delay; 
+millisDelay strobeDelay;
 int interval = 900;
+int interval2 = 500;
 
 int i = 0; // counter 
-
 
 void setup() {
     M5.begin(true,false, true); 
@@ -24,89 +25,154 @@ void CheckDelay(){ // functions that checks if delay ended and sets screen to bl
   if(Delay.justFinished())
   {
     Delay.repeat();
-    M5.dis.fillpix(black);
+    M5.dis.clear();
     i++; // increments counter
   } 
 }
 void loop() { 
     switch (i){
       case 0: 
-          M5.dis.drawpix(12,blue1); // sets the center pixel to the color we choose
-          CheckDelay(); // calls function to check the delay
-          break;
-      case 1:
-          M5.dis.drawpix(7,blue1);
-          M5.dis.drawpix(11,blue1);
-          M5.dis.drawpix(13,blue1);
-          M5.dis.drawpix(17,blue1);
-          CheckDelay();
-          break;
+        M5.dis.drawpix(12,L1color); // sets the center pixel to the color we choose
+        CheckDelay(); // calls function to check the delay
+        break;
+      case 1: //Inner diamond
+        M5.dis.drawpix(7,L2color);
+        M5.dis.drawpix(11,L2color);
+        M5.dis.drawpix(13,L2color);
+        M5.dis.drawpix(17,L2color);
+        CheckDelay();
+        break;
       case 2:
-          M5.dis.drawpix(2,blue1);
-          M5.dis.drawpix(6,blue1);
-          M5.dis.drawpix(8,blue1);
-          M5.dis.drawpix(10,blue1);
-          M5.dis.drawpix(14,blue1);
-          M5.dis.drawpix(16,blue1);
-          M5.dis.drawpix(18,blue1);
-          M5.dis.drawpix(22,blue1);
-          CheckDelay();
-          break;
+        M5.dis.drawpix(2,L3color);
+        M5.dis.drawpix(6,L3color);
+        M5.dis.drawpix(8,L3color);
+        M5.dis.drawpix(10,L3color);
+        M5.dis.drawpix(14,L3color);
+        M5.dis.drawpix(16,L3color);
+        M5.dis.drawpix(18,L3color);
+        M5.dis.drawpix(22,L3color);
+        CheckDelay();
+        break;
       case 3: 
-          M5.dis.drawpix(1,blue1);
-          M5.dis.drawpix(5,blue1);
-          M5.dis.drawpix(3,blue1);
-          M5.dis.drawpix(9,blue1);
-          M5.dis.drawpix(15,blue1);
-          M5.dis.drawpix(21,blue1);
-          M5.dis.drawpix(23,blue1);
-          M5.dis.drawpix(19,blue1);
-          CheckDelay();
-          break;
+        M5.dis.drawpix(1,L4color);
+        M5.dis.drawpix(5,L4color);
+        M5.dis.drawpix(3,L4color);
+        M5.dis.drawpix(9,L4color);
+        M5.dis.drawpix(15,L4color);
+        M5.dis.drawpix(19,L4color);
+        M5.dis.drawpix(21,L4color);
+        M5.dis.drawpix(23,L4color);
+        CheckDelay();
+        break;
       case 4: 
-          M5.dis.drawpix(0,blue1);
-          M5.dis.drawpix(4,blue1);
-          M5.dis.drawpix(20,blue1);
-          M5.dis.drawpix(24,blue1);
-          CheckDelay();
-          break;
+        M5.dis.drawpix(0,L5color);
+        M5.dis.drawpix(4,L5color);
+        M5.dis.drawpix(20,L5color);
+        M5.dis.drawpix(24,L5color);
+        CheckDelay();
+        break;
       case 5:
-          M5.dis.drawpix(0,blue1);
-          M5.dis.drawpix(1,blue1);
-          M5.dis.drawpix(2,blue1);
-          M5.dis.drawpix(3,blue1);
-          M5.dis.drawpix(4,blue1);
-          M5.dis.drawpix(5,blue1);
-          M5.dis.drawpix(9,blue1);
-          M5.dis.drawpix(10,blue1);
-          M5.dis.drawpix(14,blue1);
-          M5.dis.drawpix(15,blue1);
-          M5.dis.drawpix(19,blue1);
-          M5.dis.drawpix(10,blue1);
-          M5.dis.drawpix(20,blue1);
-          M5.dis.drawpix(21,blue1);
-          M5.dis.drawpix(22,blue1);
-          M5.dis.drawpix(23,blue1);
-          M5.dis.drawpix(24,blue1);
-          CheckDelay();
+        M5.dis.drawpix(0,L5color);
+        M5.dis.drawpix(1,L5color);
+        M5.dis.drawpix(2,L5color);
+        M5.dis.drawpix(3,L5color);
+        M5.dis.drawpix(4,L5color);
+        M5.dis.drawpix(5,L5color);
+        M5.dis.drawpix(9,L5color);
+        M5.dis.drawpix(10,L5color);
+        M5.dis.drawpix(14,L5color);
+        M5.dis.drawpix(15,L5color);
+        M5.dis.drawpix(19,L5color);
+        M5.dis.drawpix(20,L5color);
+        M5.dis.drawpix(21,L5color);
+        M5.dis.drawpix(22,L5color);
+        M5.dis.drawpix(23,L5color);
+        M5.dis.drawpix(24,L5color);
+        M5.dis.drawpix(6,L3color);
+        M5.dis.drawpix(7,L3color);
+        M5.dis.drawpix(8,L3color);
+        M5.dis.drawpix(11,L3color);
+        M5.dis.drawpix(13,L3color);
+        M5.dis.drawpix(16,L3color);
+        M5.dis.drawpix(17,L3color);
+        M5.dis.drawpix(18,L3color);
+        M5.dis.drawpix(12,L1color);
+        CheckDelay();
+        break;
       case 6:
-          M5.dis.drawpix(6,blue1);
-          //M5.dis.drawpix(7,blue1);
-          //M5.dis.drawpix(8,blue1);
-          //M5.dis.drawpix(11,blue1);
-          //M5.dis.drawpix(13,blue1);
-          //M5.dis.drawpix(16,blue1);
-          //M5.dis.drawpix(17,blue1);
-          //M5.dis.drawpix(18,blue1);
-          //M5.dis.drawpix(19,blue1);
-          CheckDelay();
+        M5.dis.clear();
+        CheckDelay();
+        break;  
       case 7:
-          //M5.dis.drawpix(12,blue1);
-          CheckDelay();
+        M5.dis.drawpix(0,L5color);
+        M5.dis.drawpix(1,L5color);
+        M5.dis.drawpix(2,L5color);
+        M5.dis.drawpix(3,L5color);
+        M5.dis.drawpix(4,L5color);
+        M5.dis.drawpix(5,L5color);
+        M5.dis.drawpix(9,L5color);
+        M5.dis.drawpix(10,L5color);
+        M5.dis.drawpix(14,L5color);
+        M5.dis.drawpix(15,L5color);
+        M5.dis.drawpix(19,L5color);
+        M5.dis.drawpix(20,L5color);
+        M5.dis.drawpix(21,L5color);
+        M5.dis.drawpix(22,L5color);
+        M5.dis.drawpix(23,L5color);
+        M5.dis.drawpix(24,L5color);
+        M5.dis.drawpix(6,L3color);
+        M5.dis.drawpix(7,L3color);
+        M5.dis.drawpix(8,L3color);
+        M5.dis.drawpix(11,L3color);
+        M5.dis.drawpix(13,L3color);
+        M5.dis.drawpix(16,L3color);
+        M5.dis.drawpix(17,L3color);
+        M5.dis.drawpix(18,L3color);
+        M5.dis.drawpix(12,L1color);
+        CheckDelay();
+        break;
+      case 8:
+        M5.dis.drawpix(0,L5color);
+        M5.dis.drawpix(1,L5color);
+        M5.dis.drawpix(2,L5color);
+        M5.dis.drawpix(3,L5color);
+        M5.dis.drawpix(4,L5color);
+        M5.dis.drawpix(5,L5color);
+        M5.dis.drawpix(9,L5color);
+        M5.dis.drawpix(10,L5color);
+        M5.dis.drawpix(14,L5color);
+        M5.dis.drawpix(15,L5color);
+        M5.dis.drawpix(19,L5color);
+        M5.dis.drawpix(20,L5color);
+        M5.dis.drawpix(21,L5color);
+        M5.dis.drawpix(22,L5color);
+        M5.dis.drawpix(23,L5color);
+        M5.dis.drawpix(24,L5color);
+        CheckDelay();
+        break;
+      case 9:
+        M5.dis.drawpix(6,L3color);
+        M5.dis.drawpix(7,L3color);
+        M5.dis.drawpix(8,L3color);
+        M5.dis.drawpix(11,L3color);
+        M5.dis.drawpix(13,L3color);
+        M5.dis.drawpix(16,L3color);
+        M5.dis.drawpix(17,L3color);
+        M5.dis.drawpix(18,L3color);
+        CheckDelay();
+        break;
+      case 10: 
+        M5.dis.drawpix(12,L1color);
+        CheckDelay();
+        break;
+      case 11:
+        M5.dis.clear();
+        CheckDelay();
+        break;   
       default:
-          break;
+        break;
     }
     
-    if(i == 8) 
+    if(i == 12) 
       i = 0; // resets counter
 }
