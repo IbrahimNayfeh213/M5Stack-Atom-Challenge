728497815cf2d6901104330581d2e25757fa454a
G-Kebede <65799321+G-Kebede@users.noreply.github.com>
2021-06-07 05:48:47 +0400

    Worked on main menu, unsolved issue with text and display libraries.

--- TempSensor/mainmenu_1.cpp
+++ TempSensor/mainmenu_1.cpp
@@ -1,128 +1,103 @@
+#include <Arduino.h>
 #include "M5Atom.h"
-#include "millisDelay.h"
-#include <Adafruit_GFX.h>
-#include <Adafruit_NeoMatrix.h>
-#include <Adafruit_NeoPixel.h>
 
-#define PIN 27
+//Set up accel varriables 
+float accX_avg = 0, accY_avg = 0, accZ_avg = 0;
+int n_average = 15;
+bool IMU_ready = false;
 
-Adafruit_NeoMatrix matrix = Adafruit_NeoMatrix(5, 5, PIN,
-  NEO_MATRIX_TOP     + NEO_MATRIX_LEFT +
-  NEO_MATRIX_ROWS + NEO_MATRIX_PROGRESSIVE,
-  NEO_GRB            + NEO_KHZ800);
+//Returns screen to black after time
+unsigned long display_off_millis;
 
-/*variable declaration*/
-String menuMode = "0"; 
-int ORNT = 0;
-int x    = matrix.width();
-int pass = 0;
-const uint16_t textWhite = matrix.Color(255,255,255);
-uint32_t white = 0xffffff; 
-uint32_t red = 0x00ff00;
+// timer for the reaction time
+unsigned long response_until_time;
 
-/*Storing data from sensors*/
-float gyroX = 0, gyroY = 0, gyroZ = 0;
-float accX = 0, accY = 0, accZ = 0;
-float temp = 0;
+// timer for wait a while
+unsigned long wait_millis;
 
-/*Running average of acceleration values*/
-int n_vals = 10;
-float accX_avg = ((accX_avg * (n_vals-1))+accX)/n_vals;
+int game_state = s_START;
 
-///*function to display values onto the Atom*/
-//void dispMode(){
-//  matrix.fillScreen(0);
-//  matrix.setCursor(x, 0);
-//  matrix.print(F(menMode));
-//  matrix.show();
-//  delay(100);
-//}
+#define CENTER 0
+#define LEFT 1
+#define RIGHT 2
+// if the device was tilted to the left or to the right
+int tilt_move = CENTER;
+
+// function to display a number on the LED Matrix screen
+// the characters are defined in the fiel "characters.c"
+void display_number(uint8_t number) {
+    // only single digit numbers between 0 and 9 are possible.
+    if (number < 20) {
+        M5.dis.displaybuff((uint8_t*)image_numbers[number]);
+    }
+    else {
+        M5.dis.displaybuff((uint8_t*)image_dot);
+    }
+}
 
 void setup() {
-  M5.begin(true,false, true);
-  M5.IMU.Init();
-  M5.dis.clear();
-  matrix.begin();
-  matrix.setTextWrap(false);
-  matrix.setBrightness(20);
-  matrix.setTextColor(textWhite);
+    // start the ATOM device with serial and Display
+    // begin(SerialEnable, I2CEnable, DisplayEnable)
+    M5.begin(true, false, true);
+    delay(50);
+
+    // Average the acceleration data, simple "running average" method without storng the data in an array
+    accX_avg = ((accX_avg * (n_average - 1)) + accX) / n_average;
+    accY_avg = ((accY_avg * (n_average - 1)) + accY) / n_average;
+    accZ_avg = ((accZ_avg * (n_average - 1)) + accZ) / n_average;
+
+    // check if IMU is ready
+//    if (M5.IMU.Init() == 0){
+//        IMU_ready = true;
+//        Serial.println("[OK] INIT ready");
+//        M5.dis.displaybuff((uint8_t *)image_CORRECT);
+//    } else {
+//        IMU_ready = false;
+//        Serial.println("[ERR] IMU INIT failed!");
+//        M5.dis.displaybuff((uint8_t *)image_WRONG);
+//    }
+    delay(1000);
+    display_off_millis = millis() + 1000;
 }
 
 void loop() {
-  matrix.fillScreen(0);
-  matrix.setCursor(x, 0);
-  // get the acceleration data
-  // accX = right pointing vector
-  // accy = backward pointing vector
-  // accZ = upward pointing vector
-  M5.IMU.getAccelData(&accX, &accY, &accZ);
-  M5.IMU.getTempData(&temp);
-  
-  if(M5.Btn.wasPressed()){
-    M5.dis.fillpix(white);
-    delay(500);
-    switch (ORNT){
-      case 0: // Show Current Temp & Unit
-            matrix.fillScreen(0);
-            matrix.setCursor(x, 0);
-            matrix.print(F("1"));
-            matrix.show();
-            delay(100);;
-          if(M5.Btn.wasPressed()){
-            matrix.fillScreen(0);
-            matrix.setCursor(x, 0);
-            matrix.print(F("TEMP: "));
-            matrix.print(temp);
-            matrix.print(F(" C"));
-            if(--x < -96) {
-              x = matrix.width();
-            }
-          }
-          break;
-      case 1: // Show Average of past 24H (or less)
-            matrix.fillScreen(0);
-            matrix.setCursor(x, 0);
-            matrix.print(F("2"));
-            matrix.show();
-            delay(100);;          break;
-      case 2: // Color scale & Temp range; Current Color
-            matrix.fillScreen(0);
-            matrix.setCursor(x, 0);
-            matrix.print(F("3"));
-            matrix.show();
-            delay(100);;          break;
-      case 3: // Graph of Temp
-            matrix.fillScreen(0);
-            matrix.setCursor(x, 0);
-            matrix.print(F("4"));
-            matrix.show();
-            delay(100);;          break;
-      case 4: // Change Unit
-            matrix.fillScreen(0);
-            matrix.setCursor(x, 0);
-            matrix.print(F("5"));
-            matrix.show();
-            delay(100);;          break; 
-      default:
-          break;
+    // if display_off time is reached, switch the display to black
+    if (millis() > display_off_millis) {
+        fillScreen(cBLACK);
     }
-        
-   if(abs(accX_avg) > 0.5){ // 0.5g = 45deg tilt angle
-      if(accX_avg < -0.5){
-        ORNT--;
-      }
-      if(accX_avg > 0.5){
-        ORNT++;
-      }
-      if (ORNT > 5)
-      {
-        ORNT = 0;
-      }
-   }
+    // get the acceleration data
+    // accX = right pointing vector
+    //      tilt to the right: > 0
+    //      tilt to the left:  < 0
+    // accy = backward pointing vector
+    //      tilt forward:  < 0
+    //      tilt backward: > 0
+    // accZ = upward pointing vector
+    //      flat orientation: -1g
+    float accX = 0, accY = 0, accZ = 0;
+    M5.IMU.getAccelData(&accX, &accY, &accZ);
+    checkTilt();
+    if (tilt_move == LEFT) {
+        //ctr--
+    }
+    if (tilt_move == RIGHT) {
+        //ctr++
+    }
 
-  }else {
-    M5.dis.clear();
-  }
-  
-  M5.update();
+}
+
+void checkTilt() {
+    if (abs(accX_avg) > 0.8 || abs(accY_avg) > 0.8 || abs(accZ_avg) > 0.8) {
+        if (abs(accX_avg) > 0.5) { // 0.5g = 45deg tilt angle
+            if (accX_avg < -0.5) {
+                tilt_move = LEFT;
+            }
+            if (accX_avg > 0.5) {
+                tilt_move = RIGHT;
+            }
+        }
+    }
+    else {
+        fillScreen(cBLACK);
+    }
 }
