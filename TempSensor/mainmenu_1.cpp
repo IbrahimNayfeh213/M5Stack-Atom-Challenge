97a3b568bafbea1af283afd186b04df954b4d0b4
G-Kebede <65799321+G-Kebede@users.noreply.github.com>
2021-06-07 05:52:24 +0400

    Adressed library clash. Mode is now accesible, but sensors are still not responding appropriately.

--- TempSensor/mainmenu_1.cpp
+++ TempSensor/mainmenu_1.cpp
@@ -1,32 +1,35 @@
 #include <Arduino.h>
 #include "M5Atom.h"
+#include "millisDelay.h"
+#include "characters.c"
+//bool IMU_ready = false;
 
 //Set up accel varriables 
+float accX = 0, accY = 0, accZ = 0;
 float accX_avg = 0, accY_avg = 0, accZ_avg = 0;
 int n_average = 15;
-bool IMU_ready = false;
 
-//Returns screen to black after time
-unsigned long display_off_millis;
-
-// timer for the reaction time
-unsigned long response_until_time;
-
-// timer for wait a while
-unsigned long wait_millis;
-
-int game_state = s_START;
-
+//Define tilt options and orient Matrix
 #define CENTER 0
 #define LEFT 1
 #define RIGHT 2
-// if the device was tilted to the left or to the right
 int tilt_move = CENTER;
 
-// function to display a number on the LED Matrix screen
-// the characters are defined in the fiel "characters.c"
+//Define counter variables 
+int modeCtr = 0;
+
+//Define colors
+uint32_t white = 0xffffff;
+uint32_t red = 0x00ff00;
+uint32_t black = 0x0000;
+#define cBLACK 0
+
+//Set up wait for change
+unsigned long interval = 6000;
+millisDelay waitDelay;
+
+//Display numbers of modes
 void display_number(uint8_t number) {
-    // only single digit numbers between 0 and 9 are possible.
     if (number < 20) {
         M5.dis.displaybuff((uint8_t*)image_numbers[number]);
     }
@@ -35,57 +38,7 @@ void display_number(uint8_t number) {
     }
 }
 
-void setup() {
-    // start the ATOM device with serial and Display
-    // begin(SerialEnable, I2CEnable, DisplayEnable)
-    M5.begin(true, false, true);
-    delay(50);
-
-    // Average the acceleration data, simple "running average" method without storng the data in an array
-    accX_avg = ((accX_avg * (n_average - 1)) + accX) / n_average;
-    accY_avg = ((accY_avg * (n_average - 1)) + accY) / n_average;
-    accZ_avg = ((accZ_avg * (n_average - 1)) + accZ) / n_average;
-
-    // check if IMU is ready
-//    if (M5.IMU.Init() == 0){
-//        IMU_ready = true;
-//        Serial.println("[OK] INIT ready");
-//        M5.dis.displaybuff((uint8_t *)image_CORRECT);
-//    } else {
-//        IMU_ready = false;
-//        Serial.println("[ERR] IMU INIT failed!");
-//        M5.dis.displaybuff((uint8_t *)image_WRONG);
-//    }
-    delay(1000);
-    display_off_millis = millis() + 1000;
-}
-
-void loop() {
-    // if display_off time is reached, switch the display to black
-    if (millis() > display_off_millis) {
-        fillScreen(cBLACK);
-    }
-    // get the acceleration data
-    // accX = right pointing vector
-    //      tilt to the right: > 0
-    //      tilt to the left:  < 0
-    // accy = backward pointing vector
-    //      tilt forward:  < 0
-    //      tilt backward: > 0
-    // accZ = upward pointing vector
-    //      flat orientation: -1g
-    float accX = 0, accY = 0, accZ = 0;
-    M5.IMU.getAccelData(&accX, &accY, &accZ);
-    checkTilt();
-    if (tilt_move == LEFT) {
-        //ctr--
-    }
-    if (tilt_move == RIGHT) {
-        //ctr++
-    }
-
-}
-
+//Checks if the Matrix has been tilted
 void checkTilt() {
     if (abs(accX_avg) > 0.8 || abs(accY_avg) > 0.8 || abs(accZ_avg) > 0.8) {
         if (abs(accX_avg) > 0.5) { // 0.5g = 45deg tilt angle
@@ -97,7 +50,57 @@ void checkTilt() {
             }
         }
     }
-    else {
-        fillScreen(cBLACK);
+    //  else {
+    //    M5.dis.clear();
+    //  }
+}
+
+void checkDelay() {
+    if (waitDelay.justFinished()) {
+        M5.dis.clear();
+    }
+    else {}
+}
+
+void checkCtr() {
+    if (tilt_move == LEFT) {
+        modeCtr--;
+    }
+    if (tilt_move == RIGHT) {
+        modeCtr++;
+    }
+}
+
+void setup() {
+    M5.begin(true, false, true);
+    // Average the acceleration data, simple "running average" method without storng the data in an array
+    accX_avg = ((accX_avg * (n_average - 1)) + accX) / n_average;
+    accY_avg = ((accY_avg * (n_average - 1)) + accY) / n_average;
+    accZ_avg = ((accZ_avg * (n_average - 1)) + accZ) / n_average;
+}
+
+void loop() {
+    M5.IMU.getAccelData(&accX, &accY, &accZ);
+    checkTilt();
+    checkCtr();
+    switch (modeCtr) {
+    case 0: //Passive read
+        display_number(modeCtr);
+        break;
+    case 1: //Show active temp
+        display_number(modeCtr);
+        break;
+    case 2: //Avg of 24 hours
+        display_number(modeCtr);
+        break;
+    case 3: //Color scale of temp and current temp color
+        display_number(modeCtr);
+        break;
+    case 4: //Graph of temp across range
+        display_number(modeCtr);
+        break;
+    case 5: //Change units 
+        display_number(modeCtr);
+        break;
     }
 }
