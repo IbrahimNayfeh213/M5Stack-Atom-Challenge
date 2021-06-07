3e89642965e163c88e2431dcdc3888e60845ea70
G-Kebede <65799321+G-Kebede@users.noreply.github.com>
2021-06-08 02:36:49 +0400

    Got screen to turn on/off based on orientation and button click. Need to integrate mode change on tilt.

--- TempSensor/mainmenu_1.cpp
+++ TempSensor/mainmenu_1.cpp
@@ -1,106 +1,125 @@
 #include <Arduino.h>
 #include "M5Atom.h"
 #include "millisDelay.h"
-#include "characters.c"
+#include "characters.c" //From tilt game
 //bool IMU_ready = false;
 
 //Set up accel varriables 
 float accX = 0, accY = 0, accZ = 0;
-float accX_avg = 0, accY_avg = 0, accZ_avg = 0;
-int n_average = 15;
 
 //Define tilt options and orient Matrix
-#define CENTER 0
-#define LEFT 1
-#define RIGHT 2
-int tilt_move = CENTER;
+int tilt_check = 0;
 
 //Define counter variables 
-int modeCtr = 0;
+int modeCtr = 1;
+
+//Display on check
+bool screenOn = false;
+
+//Confirm tilt direction
+int changeMenu = 0;
 
 //Define colors
-uint32_t white = 0xffffff;
-uint32_t red = 0x00ff00;
-uint32_t black = 0x0000;
+uint32_t white = 0xffffff; 
+uint32_t red = 0x00ff00; 
+uint32_t black = 0x0000; 
 #define cBLACK 0
 
 //Set up wait for change
-unsigned long interval = 6000;
+unsigned long interval = 500; 
 millisDelay waitDelay;
 
 //Display numbers of modes
-void display_number(uint8_t number) {
-    if (number < 20) {
-        M5.dis.displaybuff((uint8_t*)image_numbers[number]);
-    }
-    else {
-        M5.dis.displaybuff((uint8_t*)image_dot);
-    }
+void display_number(uint8_t number){
+  if(number < 20){
+    M5.dis.displaybuff((uint8_t *)image_numbers[number]);
+  } else {
+    M5.dis.displaybuff((uint8_t *)image_dot);
+  }
 }
 
-//Checks if the Matrix has been tilted
-void checkTilt() {
-    if (abs(accX_avg) > 0.8 || abs(accY_avg) > 0.8 || abs(accZ_avg) > 0.8) {
-        if (abs(accX_avg) > 0.5) { // 0.5g = 45deg tilt angle
-            if (accX_avg < -0.5) {
-                tilt_move = LEFT;
-            }
-            if (accX_avg > 0.5) {
-                tilt_move = RIGHT;
-            }
-        }
-    }
-    //  else {
-    //    M5.dis.clear();
-    //  }
+//Checks if the Matrix has been tilted, and in what direction
+void checkTilt(){
+  if(abs(accX) < 0.5 && abs(accY) < 0.5 && accZ > 0.5){
+    tilt_check = 1; //Face down
+  } else if (abs(accX) < 0.5 && abs(accY) < 0.5 && accZ < -0.5){
+    tilt_check = 2; //Face up
+  } else if (accX < -0.5 && abs(accY) < 0.5 && abs(accZ) < 0.5){
+    tilt_check = 3; //Tilt left
+  } else if (accX > 0.5 && abs(accY) < 0.5 && abs(accZ) < 0.5){
+    tilt_check = 4; //Tilt right
+  } else{
+    tilt_check = 0; //Error
+  }
 }
 
-void checkDelay() {
-    if (waitDelay.justFinished()) {
-        M5.dis.clear();
-    }
-    else {}
+//Turns menu on
+void checkScreenOn(){
+  if(tilt_check == 2 && M5.Btn.wasPressed()){
+    screenOn = true;
+    modeCtr = 1;
+  }
 }
 
-void checkCtr() {
-    if (tilt_move == LEFT) {
-        modeCtr--;
+void checkScreenOff(){
+  if(tilt_check == 1){
+    screenOn = false;
+    M5.dis.clear();
+    modeCtr = 0;
+  }
+}
+
+void checkDelay(){
+  if(screenOn = true && (tilt_check == 3 || tilt_check == 4)){
+    waitDelay.start(interval);
+    if(waitDelay.justFinished()){ 
+      if(tilt_check = 3){
+        changeMenu = 1;
+      } else if(tilt_check = 4){
+        changeMenu = 2;
+      }
     }
-    if (tilt_move == RIGHT) {
-        modeCtr++;
-    }
+  }
+}
+
+void checkCtr(){
+  if(changeMenu == 1){
+    modeCtr--;
+  }
+  if(changeMenu == 2){
+    modeCtr++;
+  }
 }
 
 void setup() {
-    M5.begin(true, false, true);
-    // Average the acceleration data, simple "running average" method without storng the data in an array
-    accX_avg = ((accX_avg * (n_average - 1)) + accX) / n_average;
-    accY_avg = ((accY_avg * (n_average - 1)) + accY) / n_average;
-    accZ_avg = ((accZ_avg * (n_average - 1)) + accZ) / n_average;
+  M5.begin(true, false, true);
+  M5.IMU.Init();
+  M5.dis.clear();
 }
 
 void loop() {
-    M5.IMU.getAccelData(&accX, &accY, &accZ);
-    checkTilt();
-    checkCtr();
-    switch (modeCtr) {
-    case 0: //Passive read
-        display_number(modeCtr);
-        break;
+  M5.IMU.getAccelData(&accX, &accY, &accZ);
+  checkTilt();
+  checkScreenOn();
+  checkScreenOff();
+  checkDelay();
+  checkCtr();
+  switch (modeCtr){
     case 1: //Show active temp
-        display_number(modeCtr);
-        break;
+      display_number(modeCtr);
+      break;
     case 2: //Avg of 24 hours
-        display_number(modeCtr);
-        break;
+      display_number(modeCtr);
+      break;
     case 3: //Color scale of temp and current temp color
-        display_number(modeCtr);
-        break;
+      display_number(modeCtr);
+      break;
     case 4: //Graph of temp across range
-        display_number(modeCtr);
-        break;
+      display_number(modeCtr);
+      break;
     case 5: //Change units 
-        display_number(modeCtr);
-        break;
-    }
+      display_number(modeCtr);
+      break;
+  }
+  M5.update();
 }
