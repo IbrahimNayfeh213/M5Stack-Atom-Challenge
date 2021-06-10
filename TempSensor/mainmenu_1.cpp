5f4f7f12351c15a483bd0631d96fbddebb0e1765
G-Kebede <65799321+G-Kebede@users.noreply.github.com>
2021-06-11 01:10:27 +0400

    Got the adafruit script to run. Everything is functional, for now.

--- TempSensor/mainmenu_1.cpp
+++ TempSensor/mainmenu_1.cpp
@@ -1,8 +1,21 @@
 #include <Arduino.h>
 #include "M5Atom.h"
 #include "millisDelay.h"
-#include "characters.c" //From tilt game
-//bool IMU_ready = false;
+#include "passive_temp_read.h" //Script for passive temp read
+#include "changeTemp.h" //Script for changing the temperature unit
+#include "modeCharacters.h" //Script for the non-scrolling numbers for the mode
+//
+//#include <Adafruit_GFX.h>
+//#include <Adafruit_NeoMatrix.h>
+//#include <Adafruit_NeoPixel.h>
+//
+///* Set up text display*/
+//#define PIN 27
+//
+//Adafruit_NeoMatrix matrix = Adafruit_NeoMatrix(5, 5, PIN,
+//  NEO_MATRIX_TOP     + NEO_MATRIX_LEFT +
+//  NEO_MATRIX_ROWS + NEO_MATRIX_PROGRESSIVE,
+//  NEO_GRB            + NEO_KHZ800);
 
 //Set up accel varriables
 float accX = 0, accY = 0, accZ = 0;
@@ -26,45 +39,118 @@ unsigned long tiltInterval = 750;
 float tiltTimer = 0;
 bool timeDone = false;
 
-//Display numbers of modes
-void display_number(uint8_t number) {
-  if (number < 20) {
-    M5.dis.displaybuff((uint8_t *)image_numbers[number]);
-  } else {
-    M5.dis.displaybuff((uint8_t *)image_dot);
+//Activate modes
+bool modeOn = false;
+
+void setup() {
+  M5.begin(true, false, true);
+  M5.IMU.Init();
+  matrix.clear();
+  Serial.begin(115200);
+  Serial.println("Temperature Data");
+  
+  setupPTRDelay(); //Passive Temp Read, start timer
+  setupChangeTemp(); //Initialize temp change
+}
+
+void loop() {
+  runMenu();
+  passiveTempRead();
+  checkButton();
+  matrix.show();
+}
+
+void runMenu(){
+  M5.IMU.getAccelData(&accX, &accY, &accZ);
+  checkTilt();
+  checkScreenOn();
+  checkScreenOff();
+  if (screenOn == true) {
+    checkDelay();
+    checkCtr();
+    activateMode();
+    switch (modeCtr) {
+      case 1: //Show active temp
+        drawMatrix(one, palette);
+        checkMode();
+        if(modeOn == true){
+          
+        }
+        checkMode();
+        break;
+      case 2: //Avg of 24 hours
+        drawMatrix(two, palette);
+        checkMode();
+        if(modeOn == true){
+          
+        }
+        break;
+      case 3: //Color scale of temp and current temp color
+        drawMatrix(three, palette);
+        checkMode();
+        if(modeOn == true){
+          
+        }
+        break;
+      case 4: //Graph of temp across range
+        drawMatrix(four, palette);
+        checkMode();
+        if(modeOn == true){
+          
+        }
+        break;
+      case 5: //Change units
+        drawMatrix(five, palette);
+        checkMode();
+        if(modeOn == true){
+          changeTemp();
+        }
+        checkMode();
+        break;
+    }
+  }
+  M5.update();
+}
+
+void checkMode(){
+  if(M5.Btn.wasPressed() && screenOn == true){
+    modeOn = !modeOn;
   }
 }
 
 //Checks if the Matrix has been tilted, and in what direction
 void checkTilt() {
-  if (abs(accX) < 0.5 && abs(accY) < 0.5 && accZ > 0.5) {
-    tilt_check = 1; //Face down
-  } else if (abs(accX) < 0.5 && abs(accY) < 0.5 && accZ < -0.5) {
-    tilt_check = 2; //Face up
-  } else if (accX < -0.5 && abs(accY) < 0.5 && abs(accZ) < 0.5) {
-    tilt_check = 3; //Tilt left
-  } else if (accX > 0.5 && abs(accY) < 0.5 && abs(accZ) < 0.5) {
-    tilt_check = 4; //Tilt right
-  } else {
-    tilt_check = 0; //Error
+  if(modeOn == false){
+    if (abs(accX) < 0.5 && abs(accY) < 0.5 && accZ > 0.5) {
+      tilt_check = 1; //Face down
+    } else if (abs(accX) < 0.5 && abs(accY) < 0.5 && accZ < -0.5) {
+      tilt_check = 2; //Face up
+    } else if (accX < -0.5 && abs(accY) < 0.5 && abs(accZ) < 0.5) {
+      tilt_check = 3; //Tilt left
+    } else if (accX > 0.5 && abs(accY) < 0.5 && abs(accZ) < 0.5) {
+      tilt_check = 4; //Tilt right
+    } else {
+      tilt_check = 0; //Error
+    }
   }
 }
 
 //Turns menu on
 void checkScreenOn() {
-  if (tilt_check == 2 && M5.Btn.wasPressed()) {
-    screenOn = true;
-    modeCtr = 1;
-    Serial.println("On");
+  if (tilt_check == 2){
+    if (modeOn == false && M5.Btn.wasPressed()) {
+      screenOn = true;
+      Serial.println("B");
+    }
   }
 }
 
 void checkScreenOff() {
   if (tilt_check == 1) {
     screenOn = false;
-    M5.dis.clear();
+    matrix.clear();
     modeCtr = 0;
-    Serial.println("Off");
+    Serial.println("C");
   }
 }
 
@@ -75,7 +161,6 @@ void checkDelay() {
   if (initialOrient == true) {
     while(timeDone == false){
       if (tilt_check == 3 || tilt_check == 4) {
-        Serial.println("A");
         tiltTimer = millis() + tiltInterval;
         timeDone = true;
         changeMenu = 0;
@@ -84,12 +169,9 @@ void checkDelay() {
       }
     }
     if (millis() > tiltTimer && (tilt_check == 3 || tilt_check == 4)) {
-      Serial.println("B");
       if (tilt_check == 3) {
-        Serial.println("C");
         changeMenu = 1;
       } else if (tilt_check == 4) {
-        Serial.println("D");
         changeMenu = 2;
       }
       timeDone = !timeDone;
@@ -104,7 +186,6 @@ void checkCtr() {
     modeCtr = modeCtr;
   } else {
     if (changeMenu == 1) {
-      Serial.println("F");
       if (modeCtr == 1) {
         modeCtr = 5;
       } else {
@@ -112,7 +193,6 @@ void checkCtr() {
       }
     }
     if (changeMenu == 2) {
-      Serial.println("G");
       if (modeCtr == 5) {
         modeCtr = 1;
       } else {
@@ -131,38 +211,15 @@ void activateMode() {
   }
 }
 
-void setup() {
-  M5.begin(true, false, true);
-  M5.IMU.Init();
-  M5.dis.clear();
+void checkButton(){
+  if(M5.Btn.wasPressed()){
+    Serial.print("A");
+  }
 }
 
-void loop() {
-  M5.IMU.getAccelData(&accX, &accY, &accZ);
-  checkTilt();
-  checkScreenOn();
-  checkScreenOff();
-  if (screenOn == true) {
-    checkDelay();
-    checkCtr();
-    activateMode();
-    switch (modeCtr) {
-      case 1: //Show active temp
-        display_number(modeCtr);
-        break;
-      case 2: //Avg of 24 hours
-        display_number(modeCtr);
-        break;
-      case 3: //Color scale of temp and current temp color
-        display_number(modeCtr);
-        break;
-      case 4: //Graph of temp across range
-        display_number(modeCtr);
-        break;
-      case 5: //Change units
-        display_number(modeCtr);
-        break;
-    }
-  }
-  M5.update();
+
+//Function that takes a 25 element matrix of image data and prints it on the matrix (for menu numbers) (inspiration: Mike Klepper)
+void drawMatrix(int shapeMatrix[], int colors[]) {
+  for (int i = 0; i < 25; i++)
+    matrix.setPixelColor(i, colors[shapeMatrix[i]]);
 }
