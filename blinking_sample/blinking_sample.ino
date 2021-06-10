3915c8c770ae66dc31559238a6dfd9e92c08a2b3
Bayan Assali <84909990+Bonz3@users.noreply.github.com>
2021-06-10 21:14:36 +0400

    added some comments

--- blinking_sample/blinking_sample.ino
+++ blinking_sample/blinking_sample.ino
@@ -1,23 +1,23 @@
 # include "M5Atom.h"
 # include "millisDelay.h"
-unsigned long delayStart = 0
-uint32_t red = 0x00ff00;
-unsigned long Interval = 1000; 
+unsigned long delayStart = 0 
+uint32_t red = 0x00ff00; // red color
+unsigned long Interval = 1000;
 
-bool ledOn = false;
+bool ledOn = false; // led state boolean 
 
 void setup() {
   M5.begin(true,false, true); 
 }
 
 void loop() {
-  M5.dis.fillpix(red)
+  M5.dis.fillpix(red) // sets screen to red
   ledOn = true;
-  delayStart = millis();  
+  delayStart = millis();  // starts delay
   checkLed();
 }
 
-
+/*Checks if delay ended and restarts it*/
 void checkLed() { 
   if ((millis() - delayStart) >= Interval)
   {
