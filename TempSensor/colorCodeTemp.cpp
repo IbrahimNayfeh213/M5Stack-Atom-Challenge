754e7b5164cf41675b281f3a95e8a5328598b6bc
G-Kebede <65799321+G-Kebede@users.noreply.github.com>
2021-06-11 18:10:24 +0400

    Final color coded system. Signifigantly simplified. Functional.

--- TempSensor/colorCodeTemp.cpp
+++ TempSensor/colorCodeTemp.cpp
@@ -11,158 +11,76 @@ Adafruit_NeoMatrix matrix = Adafruit_NeoMatrix(5, 5, PIN,
   NEO_MATRIX_ROWS + NEO_MATRIX_PROGRESSIVE,
   NEO_GRB            + NEO_KHZ800);
 
-/* variable declaration*/
+/* screen color values */
+uint32_t blue = 0x0000ff;
+uint32_t green = 0x00ff00;
+uint32_t yellow = 0x00ffff;
+uint32_t orange = 0xffff00;
+uint32_t red = 0xff0000;
+uint32_t crntColor;
+
 /* temp vars*/
 bool Cels = true;
 float tempVal = 0;
 float tempUnit = 0;
 
-/* text vars*/
-const uint16_t textWhite = matrix.Color(255, 255, 255);
-int x    = matrix.width();
-int pass = 0;
-
-/* screen color values */
-uint32_t blue = 0x0000ff;
-uint32_t green = 0x00ff00;
-uint32_t yellow = 0xffff00;
-uint32_t red = 0x00ff00;
-
-/* delay settings */
-unsigned long interval1 = 80;
-unsigned long loopTime1 = 0; 
-unsigned long interval2 = 70;
-unsigned long loopTime2 = 0; 
-millisDelay screenOn;
+/* changes between Celsius and Fahrenheit */
+float changeUnit(float intTemp){
+  float outTemp;
+  if(Cels == true){
+    outTemp = intTemp;
+  }else{
+    outTemp = 32 + ((9 * intTemp)/5);
+  }
+  return(outTemp);
+}
 
 /*Function that fills the screen with a certain color*/
-void fillScreen(uint32_t color){
-     M5.dis.fillpix(color);
-}
-
-/* checks what unit to display info in */
-void checkPress(){
-  if(M5.Btn.wasPressed()){
-    if(Cels == true){
-      Cels = false;
-    }else{
-      Cels = true;
-    }
-  }
-}
-
-/* changes between Celsius and Fahrenheit */
-void changeUnit(){
-  if(Cels == true){
-    tempUnit = tempVal;
-  }else{
-    tempUnit = 32 + ((9 * tempVal)/5);
-  }
-}
-
-//void checkScreenOff() { 
-//  if (ScreenOn.justFinished()) { 
-//    M5.dis.clear(); // turn leds off
-//  }
-//}
-
-/* displays the temperature ranges in C and the associated colors */
-void colorRangeC(){
-  matrix.print(F("< 15"));    //Too cold, blue
-  fillScreen(blue);
-  while(millis() < loopTime2 + interval2){}
-  loopTime2 += interval2;
-  M5.dis.clear();             // turn leds off
-  matrix.print(F("15 - 30")); //Ideal temp, green
-  fillScreen(green);
-  while(millis() < loopTime2 + interval2){}
-  loopTime2 += interval2;
-  M5.dis.clear();
-  matrix.print(F("30 - 35")); //A bit hot, yellow
-  fillScreen(yellow);
-  while(millis() < loopTime2 + interval2){}
-  loopTime2 += interval2;
-  M5.dis.clear();
-  matrix.print(F("> 35"));    //Too hot, red
-  fillScreen(red);  while(millis() < loopTime2 + interval2){}
-  loopTime2 += interval2;
-  M5.dis.clear();
-  if(--x < -96) {
-    x = matrix.width();
-  }
-  matrix.show();
+void displayTempColor(uint32_t color){
+     matrix.setPixelColor(0, blue);
+     matrix.setPixelColor(1, green);
+     matrix.setPixelColor(2, yellow);
+     matrix.setPixelColor(3, orange);
+     matrix.setPixelColor(4, red);
+     matrix.fill(color, 5, 20);
 }
 
 /* fills screen with a color based on ranges in C */
-void currentTempColorC(float curTemp){
+uint32_t currentTempColorC(float curTemp){
+  uint32_t tColor;
   if(curTemp < 15){
-    fillScreen(blue);
-  }else if (curTemp < 30){
-    fillScreen(green);
-  }else if (curTemp < 35){
-    fillScreen(yellow);
-  }else{
-    fillScreen(red);
+    tColor = blue;
+  } else if (curTemp < 30){
+    tColor = green;
+  } else if (curTemp < 35){
+    tColor = yellow;
+  } else if (curTemp < 40){
+    tColor = orange;
+  } else if (curTemp < 45){
+    tColor = red;
+  } else {
+    break;
   }
+  return(tColor);
 }
 
 void setup() {
   M5.begin(true,false, true);
-  M5.dis.clear();
-  M5.dis.setBrightness(20);
   M5.IMU.Init();
-  
+  Serial.begin(115200);
+
+  matrix.clear();
   matrix.begin();
   matrix.setTextWrap(false);
   matrix.setBrightness(20);
-  matrix.setTextColor(textWhite);
 }
+
 
 void loop() {
   M5.IMU.getTempData(&tempVal);
-  checkPress();
-  changeUnit();
-
-  colorRangeC();
-  currentTempColorC(tempUnit);
-  
-  while(millis() < loopTime1 + interval1){}
-  loopTime1 += interval1;
+  tempUnit = changeUnit(tempVal);
+  crntColor = currentTempColorC(tempUnit);
+  displayTempColor(crntColor);
+  matrix.show();
   M5.update();
 }
-
-
-///* displays the temperature ranges in F and the associated colors */
-//void colorRangeF(){
-//  matrix.print(F("< 60"));    //Too cold, blue
-//  fillScreen(blue);
-//  matrix.print(F("60 - 85")); //Ideal temp, green
-//  fillScreen(green);
-//  matrix.print(F("85 - 95")); //A bit hot, yellow
-//  fillScreen(yellow);
-//  matrix.print(F("> 95"));    //Too hot, red
-//  fillScreen(red);
-//  if(--x < -96) {
-//    x = matrix.width();
-//  }
-//  matrix.show();
-//}
-
-///* fills screen with a color based on ranges in F */
-//void currentTempColorF(float curTemp){
-//  if(curTemp < 60){
-//    fillScreen(blue);
-//  }else if (curTemp < 85){
-//    fillScreen(green);
-//  }else if (curTemp < 95){
-//    fillScreen(yellow);
-//  }else{
-//    fillScreen(red);
-//  }
-//}
-
-//  if(Cels == true){
-//    colorRangeC();
-//  }else{
-//    colorRangeF();
-//  }  
